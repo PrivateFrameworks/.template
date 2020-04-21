d7e458f746d72a502283544140fe4d979012998c
Dimitar Nestorov <dimitarnestorov@users.noreply.github.com>
2020-04-21 08:52:57 +0300

    ​

---
+++ PrivateNameOfFramework.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double NameOfFrameworkVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char NameOfFrameworkVersionString[];
---
+++ Tests/Tests.m
@@ -0,0 +1,15 @@
+#import <objc/objc-runtime.h>
+#import <XCTest/XCTest.h>
+
+#import <PrivateNameOfFramework/PrivateNameOfFramework.h>
+
+@interface PrivateNameOfFrameworkTests : XCTestCase
+
+@end
+
+@implementation PrivateNameOfFrameworkTests
+
+- (void)testNameOfClass {
+}
+
+@end
